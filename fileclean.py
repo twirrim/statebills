0f31b07f5228536a0ee5e5ed1778bd9482b1ef68
twirrim <paulgraydon@hotmail.com>
2012-12-01 21:17:31 +0000

    Don't want to deal with PDFs

--- fileclean.py
+++ fileclean.py
@@ -11,6 +11,15 @@ def sedish(filename,fromstr,tostr):
                 out.write(re.sub(fromstr,tostr,line))
     move(tempname,filename)
 
+def delline(filename):
+    tempname = "%s.tmp" % filename
+    with open(tempname, "w") as out:
+        for line in open(filename):
+            if (not "pdf" in line) and (not "PDF" in line):
+                out.write(line)
+    move(tempname,filename)
+    
+
 def cleanup(filename):
     sedish(filename,"\r\n","\n")
     sedish(filename,"<br>","\n")
@@ -19,3 +28,6 @@ def cleanup(filename):
     sedish(filename,r'^\s*(\w)',r'"\1')
     sedish(filename,r'^<.*',r'')
     sedish(filename,"","")
+    delline(filename)
+
+cleanup("data")
